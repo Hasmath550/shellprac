519d06a311863e928c3af9d91ac461b6bd89239c
hashu <hashu@550>
2026-02-19 13:33:02 +0530

    actions

---
+++ 10-functions.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+
+USERID=$(id -u)     #checking root user 
+if [ $USERID -ne 0 ]; then
+
+ echo "run with root user access"
+ 
+fi
+
+VALIDATE(){
+    if [$1 -ne 0]; then
+     echo $2,,is failure
+    else 
+     echo "s2 is success"
+    fi
+
+}
+
+dnf install nginx -y
+VALIDATE $? "install nginx"
+
+dnf install mysql -y
+VALIDATE $? "install mysql"
+
+dnf install nodejs -y
+VALIDATE $? "install nodejs"
