c1dea5976b7f315f5fa966210dfd49413b469585
hashu <hashu@550>
2026-02-19 13:51:31 +0530

    actions

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,13 +2,6 @@
 #!/bin/bash
 
 USERID=$(id -u)     #checking root user 
-if [ $USERID -ne 0 ]; then
-
- echo "run with root user access"
- 
- exit 1
- 
-fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
