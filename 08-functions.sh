747aaad17e5a00b1fb6967469006f1d287a5bfc6
hashu <hashu@550>
2026-02-19 13:18:24 +0530

    actions

--- 08-functions.sh
+++ 08-functions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-USERID=$(id -u)
+USERID=$(id -u) #checking root user 
 if [ $USERID -ne 0 ]; then
-echo "run with root user access"
+
+ echo "run with root user access"
 fi
+
 echo "install nginx"
 dnf install nginx -y
